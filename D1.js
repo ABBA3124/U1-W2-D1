3ea091b722cfcc188c7bf263a4ebdf71138eedc5
ABBA3124 <davideabbadessa73@gmail.com>
2024-03-04 15:34:19 +0100

    finito

--- D1.js
+++ D1.js
@@ -13,6 +13,16 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+/* booleani: possono essere true e false
+   numeri: Sono come i numeri che usi per contare le cose o fare operazioni matematiche.
+   stringhe: rappresentano il testo (lettere,frasi e parole)
+   null: e come se in una scatola non c'è nulla 
+   undefined: non ha un valore definito
+*/ 
+
+
+
+
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
@@ -20,6 +30,12 @@ REGOLE
 /* SCRIVI QUI LA TUA RISPOSTA */
 let myName = "davide"
 console.log (myName)
+
+
+
+
+
+
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
@@ -35,12 +51,21 @@ console.log("Il risultato dell'addizione di", number1, "e", number2, "è:", resu
 let result2 = number1 + number2
 console.log(result2)
 
+
+
+
+
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 let x = 12
+
+
+
+
+
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
@@ -49,6 +74,14 @@ let x = 12
 /* SCRIVI QUI LA TUA RISPOSTA */
 myName = "abbadessa"
 console.log(myName)
+
+const errore = 7
+// errore = 10
+console.log(errore)
+
+
+
+
 
 
 /* ESERCIZIO 6
@@ -59,9 +92,15 @@ console.log(myName)
 let number3 = 4
 let subtraction = x - number3
 console.log(subtraction)
+
 // si può anche fare:
 let subtraction2 = x - 4
 console.log(subtraction2)
+
+
+
+
+
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -72,5 +111,13 @@ console.log(subtraction2)
 /* SCRIVI QUI LA TUA RISPOSTA */
 let name1 = "john"
 let name2 = "John"
-let verify = name1 != name2
+let verify = name1 !== name2
 console.log("sono diversi:","John", "e", "john","?", verify)
+
+// EXTRA
+console.log("Sono uguali se scritti in lowercase:", name1.toLowerCase(), "e", name2.toLowerCase(), "?", verify)
+
+
+// EXTRA: ALTRO MODO PER FARLO 
+let verify2 = name1.toLowerCase() === name2.toLowerCase()
+console.log("Sono uguali se scritti in lowercase:", name1, "e", name2, "?", verify2)
